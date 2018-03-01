3b4aec131638929662fc84d040673e55384746b2
MaricruzCF <mmichel5@ucol.mx>
2018-02-28 23:27:54 -0600

    Add files via upload

---
+++ tarea_2ED/tarea_2ED/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace tarea_2ED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnleerBMP = new System.Windows.Forms.Button();
+            this.txtLeer = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnleerBMP
+            // 
+            this.btnleerBMP.Location = new System.Drawing.Point(25, 39);
+            this.btnleerBMP.Name = "btnleerBMP";
+            this.btnleerBMP.Size = new System.Drawing.Size(75, 23);
+            this.btnleerBMP.TabIndex = 6;
+            this.btnleerBMP.Text = "Leer BMP";
+            this.btnleerBMP.UseVisualStyleBackColor = true;
+            this.btnleerBMP.Click += new System.EventHandler(this.btnleerBMP_Click);
+            // 
+            // txtLeer
+            // 
+            this.txtLeer.Location = new System.Drawing.Point(25, 100);
+            this.txtLeer.Multiline = true;
+            this.txtLeer.Name = "txtLeer";
+            this.txtLeer.Size = new System.Drawing.Size(229, 103);
+            this.txtLeer.TabIndex = 5;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(157, 39);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(97, 23);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar XML";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnleerBMP);
+            this.Controls.Add(this.txtLeer);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnleerBMP;
+        private System.Windows.Forms.TextBox txtLeer;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ tarea_2ED/tarea_2ED/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace tarea_2ED
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnleerBMP_Click(object sender, EventArgs e)
+        {
+            var ancho = 0;
+            var tamanio = 0;
+            var alto = 0;
+            short pixel = 0;
+            
+            openFileDialog1.ShowDialog();
+            FileStream archivo = new FileStream(openFileDialog1.FileName, FileMode.Open);
+            BinaryReader br = new BinaryReader(archivo);
+
+            br.BaseStream.Seek(0, SeekOrigin.Begin);
+            if ("BM" != new string(br.ReadChars(2)))
+            { 
+            txtLeer.Text = ("Error");
+            return;
+            }
+            br.BaseStream.Seek(18, SeekOrigin.Begin);
+            ancho = br.ReadInt32();
+            alto = br.ReadInt32();
+
+            br.BaseStream.Seek(2, SeekOrigin.Begin);
+            tamanio = br.ReadInt32();
+
+            br.BaseStream.Seek(2, SeekOrigin.Current);
+            pixel = br.ReadInt16();
+
+            txtLeer.Text = (" Tamanio: " + tamanio + " Ancho: " + ancho + " Alto: " + alto + " Bits por Pixel: " + pixel);
+            
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            txtLeer.Clear();
+
+        }
+    }
+}
